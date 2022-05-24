a75ebe56612cf2517438568c6c5d796d9104877c
jukangpark <skyxxx9339@naver.com>
2022-05-24 18:41:20 +0900

    HTML 마크업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
-function App() {
+const Wrapper = styled.div`
+  width: 200px;
+  margin: 100px auto;
+`;
+
+const Number = styled.div`
+  text-align: center;
+  line-height: 50px;
+`;
+
+const Section = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+`;
+
+const Input = styled.input`
+  display: block;
+  width: 200px;
+  height: 50px;
+  border: 1px solid gray;
+`;
+
+const Button = styled(Number)``;
+
+const Result = styled(Number)``;
+
+const ResultSection = styled.div`
+  display: flex;
+  > div {
+    width: 25%;
+  }
+  > div:nth-child(1) {
+    width: 50%;
+  }
+`;
+
+const App = () => {
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Input value={0} onChange={handleChange} />
+      <Section>
+        <Number>7</Number>
+        <Number>8</Number>
+        <Number>9</Number>
+        <Button>X</Button>
+      </Section>
+      <Section>
+        <Number>4</Number>
+        <Number>5</Number>
+        <Number>6</Number>
+        <Button>-</Button>
+      </Section>
+      <Section>
+        <Number>1</Number>
+        <Number>2</Number>
+        <Number>3</Number>
+        <Button>+</Button>
+      </Section>
+      <ResultSection>
+        <Result>0</Result>
+        <Button>.</Button>
+        <Button>=</Button>
+      </ResultSection>
+    </Wrapper>
   );
-}
+};
 
 export default App;
